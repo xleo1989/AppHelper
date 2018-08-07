10f5b7cdd21c178c26a3175634212e0734d69750
xleo <xleo1989@gmail.com>
2018-08-07 10:23:59 +0800

    simplize

--- src/main/java/com/x/leo/apphelper/download/DemoMethod.kt
+++ src/main/java/com/x/leo/apphelper/download/DemoMethod.kt
@@ -2,8 +2,6 @@ package com.x.leo.apphelper.download
 
 import android.app.Activity
 import android.widget.Toast
-import com.tbruyelle.rxpermissions.RxPermissions
-import rx.Subscriber
 
 /**
  * Created by XLEO on 2018/4/9.
@@ -13,27 +11,16 @@ object DemoMethod {
         when (UserDownloadManager.checkExternalStorageState(activity)) {
             StorageUsableStates.OK -> showUpdate(activity)
             StorageUsableStates.EXTERNAL_STORAGE_NOT_USABLE -> Toast.makeText(activity, "no usable external storage", Toast.LENGTH_LONG).show()
-            StorageUsableStates.PERMISSION_OF_WRITE_NOT_GRANTED -> RxPermissions(activity)
-                    .request(android.Manifest.permission.WRITE_EXTERNAL_STORAGE)
-                    .subscribe(object : Subscriber<Boolean>() {
-                        override fun onCompleted() {
-
-                        }
-
-                        override fun onError(throwable: Throwable) {
-                            Toast.makeText(activity,"error happened when request permission",Toast.LENGTH_LONG).show()
-                        }
-
-                        override fun onNext(aBoolean: Boolean?) {
-                            if (aBoolean!!) {
-                                showUpdate(activity)
-                            }else{
-                                Toast.makeText(activity,"permission denied",Toast.LENGTH_LONG).show()
-                            }
-                        }
-                    })
-            else -> {
+            StorageUsableStates.PERMISSION_OF_WRITE_NOT_GRANTED -> {
+                //todo request permission first
+                val aBoolean = true
+                if (aBoolean!!) {
+                    showUpdate(activity)
+                } else {
+                    Toast.makeText(activity, "permission denied", Toast.LENGTH_LONG).show()
+                }
             }
+
         }
 
     }
